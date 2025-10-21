ea8fa781d312fa1a9bb5e83bae9e000d3eafb3c5
Matheus Diniz <matheus.gruber123@gmail.com>
2025-10-21 17:25:56 -0300

    Aqui funcionou abrir o codigo

--- frontend/src/main.server.ts
+++
@@ -1,9 +0,0 @@
-// c:\Users\matheus.diniz\Documents\GitHub\CCAT\monPonte\frontend\src\main.server.ts
-
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component'; // Certifique-se que é AppComponent
-import { config } from './app/app.config.server'; // Este arquivo deve ser gerado pelo Angular CLI para SSR
-
-const bootstrap = (context: any) => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
