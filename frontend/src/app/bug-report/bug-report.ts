bdcefebb1f7c5ac439d621664ce3fc64875a10c0
Matheus Diniz <matheus.gruber123@gmail.com>
2025-11-07 14:36:08 -0300

    bugs Report

--- frontend/src/app/bug-report/bug-report.ts
+++ frontend/src/app/bug-report/bug-report.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../layout/header/header';
+import { FooterComponent } from '../layout/footer/footer';
 
 @Component({
   selector: 'app-bug-report',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './bug-report.html',
   styleUrls: ['./bug-report.css']
 })
